5d487a2184343573afdd37a963d39dec5c1308b1
steppannws <steppannws@gmail.com>
2017-06-14 11:24:52 -0300

    Android app relauncher

--- android/src/main/java/io/stepan/RNAppRelauncher/RNAppRelauncherModule.java
+++ android/src/main/java/io/stepan/RNAppRelauncher/RNAppRelauncherModule.java
@@ -1,6 +1,13 @@
 
 package io.stepan.RNAppRelauncher;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.AlarmManager;
+import android.content.Intent;
+
+import java.lang.System;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -10,9 +17,12 @@ public class RNAppRelauncherModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
-  public RNAppRelauncherModule(ReactApplicationContext reactContext) {
+  private Activity activity;
+
+  public RNAppRelauncherModule(ReactApplicationContext reactContext, Activity activity) {
     super(reactContext);
     this.reactContext = reactContext;
+    this.activity = activity;
   }
 
   @Override
@@ -22,6 +32,15 @@ public class RNAppRelauncherModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void relaunch() {
-  	
+  	System.exit(0);
+  	/*
+  	Intent mStartActivity = new Intent(activity);
+	int mPendingIntentId = 123456;
+	PendingIntent mPendingIntent = PendingIntent.getActivity(HomeActivity.this, mPendingIntentId, mStartActivity,
+	PendingIntent.FLAG_CANCEL_CURRENT);
+	AlarmManager mgr = (AlarmManager) HomeActivity.this.getSystemService(Context.ALARM_SERVICE);
+	mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+	System.exit(0);
+  	*/
   }
 }
--- android/src/main/java/io/stepan/RNAppRelauncher/RNAppRelauncherPackage.java
+++ android/src/main/java/io/stepan/RNAppRelauncher/RNAppRelauncherPackage.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import android.app.Activity;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -12,10 +14,16 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNAppRelauncherPackage implements ReactPackage {
-  
+    private Activity activity;
+
+    public RNAppRelauncherPackage(Activity activity) {
+      super();
+      this.activity = activity;
+    }
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNAppRelauncherModule(reactContext));
+      return Arrays.<NativeModule>asList(new RNAppRelauncherModule(reactContext, activity));
     }
 
     @Override
